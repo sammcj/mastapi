b84a676cbd38b6bd242a5cbe843a15060d5ff1e8
Sam McLeod <sammcj@users.noreply.github.com>
2022-11-19 15:46:47 +1100

    upload

--- src/upload.ts
+++ src/upload.ts
@@ -1,10 +1,13 @@
 import generator from 'megalodon';
-import fs from 'fs';
 import readline from 'readline';
 
 const instanceUrl: string = process.env.INSTANCE_URL || 'https://aus.social';
 const access_token: string = process.env.MASTODON_ACCESS_TOKEN as string;
 const client = generator('mastodon', instanceUrl, access_token);
+
+function printUsage() {
+  console.log('Usage: MASTODON_ACCESS_TOKEN=12345 node upload.js <json_file_path>');
+}
 
 // prompt the user for filter file to restore
 const rl = readline.createInterface({
@@ -22,32 +25,73 @@ function isValidJSON(json: string) {
   }
 }
 
-// prompt the user for the path to a JSON file and return the files contents as a JSON object
-function promptForJSONFile(): Promise<string> {
-  return new Promise((resolve, reject) => {
-    rl.question('Enter the path to the JSON file to restore: ', (answer) => {
-      const json = fs.readFileSync(answer, 'utf8');
-      if (isValidJSON(json)) {
-        resolve(json);
-      } else {
-        reject('Invalid JSON file');
-      }
+async function getFilePath() {
+  try {
+    return new Promise((resolve, reject) => {
+      rl.question('Enter file path: ', (answer: string) => {
+        // call isValidJSON
+        if (isValidJSON(answer)) {
+          // return the JSON
+          resolve(answer);
+        }
+        reject('Invalid JSON');
+      });
     });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function uploadFilters(filePath: string) {
+  const filtersJSON = JSON.parse(JSON.stringify(filePath));
+  const filters = filtersJSON.split(',');
+  filters.forEach((filter: any) => {
+    const phrase = filter.phrase;
+    const context = filter.context;
+    const whole_word = filter.whole_word;
+    const expires_at = filter.expires_at;
+    const irreversible = filter.irreversible;
+    const options = {
+      whole_word,
+      expires_at,
+      irreversible,
+    };
+    console.log(filters);
+    client.createFilter(phrase, context, options);
   });
 }
 
-async function uploadFilters(json: string) {
-  const filterObj = JSON.parse(json);
+if (process.argv.length > 2) {
+  const filePath = process.argv[2];
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
-  filterObj.forEach((filter: Entity.Filter) => {
-    client.createFilter(filter.phrase, filter.context);
+  rl.question(`Use ${filePath}? (y/n) `, (answer: string) => {
+    if (answer === 'y' || answer === 'Y') {
+      try {
+        console.log(uploadFilters(JSON.stringify(filePath)));
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      getFilePath().then((answer) => {
+        try {
+          uploadFilters(JSON.stringify(answer));
+        } catch (error) {
+          console.error(error);
+        }
+      });
+    }
+  });
+} else {
+  printUsage();
+  getFilePath().then((answer) => {
+    try {
+      uploadFilters(JSON.stringify(answer));
+    } catch (error) {
+      console.error(error);
+    }
   });
 }
-
-function run() {
-  promptForJSONFile().then((json) => {
-    uploadFilters(json);
-  });
-}
-
-run();
